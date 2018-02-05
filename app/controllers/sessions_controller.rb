6ef13023d36d4239ddd37f5d8f86a11848773cf5
Sébastien DEVAUX <devsebastien@hotmail.fr>
2018-02-05 21:53:41 +0100

    [MAJ] CSS

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,6 +6,7 @@ class SessionsController < ApplicationController
     user = User.find_by(email: params[:session][:email])
       if user && user.authenticate(params[:session][:password])
         log_in(user)
+        flash[:success] = "Bienvenue dans l'app secrète"
         redirect_to user
       else
         flash.now[:danger] = "Mauvaise combinaison email/password"
