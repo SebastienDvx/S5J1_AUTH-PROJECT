008663740ad786e4621e785da31dfc229eaa526e
Sébastien DEVAUX <devsebastien@hotmail.fr>
2018-02-05 19:48:41 +0100

    [ADD] Sessions OK ! + Bootstrap

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,10 +3,19 @@ class SessionsController < ApplicationController
   end
 
   def create
-
+    user = User.find_by(email: params[:session][:email])
+      if user && user.authenticate(params[:session][:password])
+        log_in(user)
+        redirect_to user
+      else
+        flash.now[:danger] = "Mauvaise combinaison email/password"
+        render 'new'
+      end
   end
 
   def destroy
+    log_out
+    redirect_to root_url
   end
 
   private
