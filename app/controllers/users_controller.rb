fe5a0e7e4451f2c29c0576ba088db2b292656061
Sébastien DEVAUX <devsebastien@hotmail.fr>
2018-02-06 00:02:31 +0100

    [DEL] index

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -29,9 +29,9 @@ class UsersController < ApplicationController
   end
 
 
-  def index
-    @users = User.all
-  end
+  # def index
+  #   @users = User.all
+  # end
 
   def edit
     @user = User.find(params[:id])
